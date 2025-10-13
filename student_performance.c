5c4555920d3c3b668f86701ea8bf50f79a0de82a
srinivasa-1833 <sinivassrini1833@gmail.com>
2025-10-13 14:37:56 +0530

    Added Student Performance Analyzer assignment with validations and modular structure

--- student_performance.c
+++ student_performance.c
@@ -44,34 +44,65 @@ void printRollNumbers(int n) {
     printf("%d ", n);
 }
 
-int main() {
-    int n;
-    scanf("%d", &n);
-    struct Student s[n];
-
-    for (int i = 0; i < n; i++) {
+void inputStudents(struct Student s[], int studentCount) {
+    for (int i = 0; i < studentCount; i++) {
         scanf("%d %s %d %d %d", &s[i].roll, s[i].name, &s[i].marks[0], &s[i].marks[1], &s[i].marks[2]);
+
+        if (s[i].roll <= 0) {
+            printf("Invalid roll number. Skipping student.\n");
+            continue;
+        }
+
+        int valid = 1;
+        for (int j = 0; j < 3; j++) {
+            if (s[i].marks[j] < 0 || s[i].marks[j] > 100) {
+                valid = 0;
+                break;
+            }
+        }
+
+        if (!valid) {
+            printf("Invalid marks for student %s. Skipping entry.\n", s[i].name);
+            continue;
+        }
+
         calculate(&s[i]);
     }
+}
 
-    for (int i = 0; i < n; i++) {
+void displayStudents(struct Student s[], int studentCount) {
+    for (int i = 0; i < studentCount; i++) {
+        if (s[i].roll <= 0) continue;
         printf("Roll: %d\n", s[i].roll);
         printf("Name: %s\n", s[i].name);
         printf("Total: %d\n", s[i].total);
         printf("Average: %.2f\n", s[i].average);
         printf("Grade: %c\n", s[i].grade);
 
-        if (s[i].average < 35) {
-            continue;
-        }
+        if (s[i].average < 35) continue;
 
         printf("Performance: ");
         performance(s[i].grade);
-        printf("\n");
+        printf("\n\n");
+    }
+}
+
+int main() {
+    int studentCount;
+    scanf("%d", &studentCount);
+
+    if (studentCount <= 0 || studentCount > 100) {
+        printf("Invalid number of students.\n");
+        return 0;
     }
 
+    struct Student s[studentCount];
+    inputStudents(s, studentCount);
+    displayStudents(s, studentCount);
+
     printf("List of Roll Numbers (via recursion): ");
-    printRollNumbers(n);
+    printRollNumbers(studentCount);
     printf("\n");
+
     return 0;
 }
